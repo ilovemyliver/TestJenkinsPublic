8ae7c86fefe4d888c141e6416a198ea40dd3f344
ilovemyliver <jonnysemailadress@gmail.com>
2021-04-28 16:20:57 -0400

    Bad code

--- HelloWorldSolution/HelloWorld/Program.cs
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
+using System.Data;
+
 
 namespace HelloWorld
 {
     public class Program
     {
+
+        static int test;
+
+        string testString;
+
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int testInside;
+
+            for (var i = 0; i < 10; i++)
+            { 
+                test++;
+            }
+
+            Console.WriteLine("Test");
         }
     }
 }
